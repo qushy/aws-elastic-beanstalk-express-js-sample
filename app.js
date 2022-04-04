56ebc075ab68631a616ef22e081c5c5657733e11
qushy <qushaoyan1010@sina.com>
2022-04-04 20:23:35 +0800

    changed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! I am fay, smart'));
+app.get('/', (req, res) => res.send('---------have a test------Hello World! I am fay, smar------t'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
